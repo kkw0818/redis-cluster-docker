0b2cdc6ebbfa1336c93976309dcb53144700dc2d
kkw0818 <kkw0818@gmail.com>
2021-04-12 23:40:55 +0900

    도커 수정 사항들.... 졸리다

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ MAINTAINER kkw0818 (kkw0818@gmail.com)
 ## 복사/추가 하는파일의 Container내 경로는 항상 절대경로로 작성하여야 한다.
 RUN rm -rf /usr/local/bin/docker-entrypoint.sh
 ADD redis.conf /etc/redis/redis.conf
-ADD docker-entrypoint.sh /usr/local/bin
+ADD docker-entrypoint.sh /usr/local/bin/docker-entrypoint.sh
 
 ## change access authority
 RUN chmod 755 /etc/redis/redis.conf
@@ -14,7 +14,7 @@ RUN chmod 755 /usr/local/bin/docker-entrypoint.sh
 
 RUN chown redis:redis /etc/redis/redis.conf
 RUN chown redis:redis /usr/local/bin/docker-entrypoint.sh
+RUN chmod +x /usr/local/bin/docker-entrypoint.sh
 
-EXPOSE $CLIENTPORT
 ENTRYPOINT ["/usr/local/bin/docker-entrypoint.sh"]
 CMD [ "redis-server","/etc/redis/redis.conf" ]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,14 +2,12 @@
 set -e
 
 ### redis port inside redis.conf
-sed -i "s/port 6379/port $CLIENTPORT" /etc/redis/redis.conf
 sed -i "s/# requirepass foobared/requirepass $REQUIREPASS/g" /etc/redis/redis.conf
 sed -i "s/# masterauth <master-password>/masterauth $REQUIREPASS/g" /etc/redis/redis.conf
-
-### slaveof <masterip> <masterport> => slaveof $MASTERHOST $MASTERPORT
-if [ "$MASTERPORT" != "" ];then
-	sed -i "s/# slaveof <masterip> <masterport>/slaveof $MASTERHOST $MASTERPORT/g" /etc/redis/redis.conf
-fi
+sed -i "s/port 6379/port $CLIENTPORT/g" /etc/redis/redis.conf
+sed -i "s/# cluster-enabled yes/cluster-enabled yes/g" /etc/redis/redis.conf
+sed -i "s/# cluster-config-file nodes-6379.conf/cluster-config-file nodes.conf/g" /etc/redis/redis.conf
+sed -i "s/# cluster-node-timeout 15000/cluster-node-timeout 5000/g" /etc/redis/redis.conf
 
 # first arg is `-f` or `--some-option`
 # or first arg is `something.conf`
@@ -20,7 +18,7 @@ fi
 # allow the container to be started with `--user`
 if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
 	chown -R redis .
-	exec su-exec redis "$@"
+    exec su-exec redis "$@"
 fi
 
 exec "$@"
